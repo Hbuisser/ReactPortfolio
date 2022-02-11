267ef4033a487ed62195eb0f9bd7602341d6b9c1
Henry Buisseret <henrybuisseret@MacBook-Pro-de-Henry.local>
2022-02-11 11:19:10 +0100

    intro ok

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,38 +1,40 @@
 import "./intro.scss";
-import { init } from 'ityped';
-import { useEffect, useRef } from 'react';
-
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
-    const textRef = useRef();
+	const textRef = useRef();
 
-    useEffect(() => {
-        init(textRef.current, { 
-            showCursor: false,
-            backDelay: 1500,
-            backspeed: 80,
-            strings: ['Mongo DB', 'Express', 'React', 'Node.js'] 
-        })
-    }, [])
+	useEffect(() => {
+		init(textRef.current, {
+			showCursor: false,
+			backDelay: 1500,
+			backspeed: 80,
+			strings: ["Mongo DB", "Express", "React", "Node.js"],
+		});
+	}, []);
 
-    return (
-        <div className="intro" id="intro">
-            <div className="left">
-                <div className="imageContainer">
-                    <img src="./assets/profil_montagne.jpg" alt=""/>
-                </div>
-            </div>
-            <div className="right">
-                <div className="wrapper">
-                    <h2>Hi there, I am</h2>
-                    <h1>Henry Buisseret</h1>
-                    <h3>MERN Expert </h3>
-                    <span ref={textRef}></span>
-                </div>
-                <a href="#portfolio">
-                    <img src="../../assets/down.png" alt=""/>
-                </a>
-            </div>
-        </div>
-    )
+	return (
+		<div className="intro" id="intro">
+			<div className="left">
+				<div className="imageContainer">
+					<img src="./assets/profil_montagne.jpg" alt="" />
+				</div>
+			</div>
+			<div className="right">
+				<div className="wrapper">
+					<div className="desc">
+						<h2>Hi there, I am</h2>
+						<h1>Henry Buisseret</h1>
+						<h3>I help you to create <span>innovative tech products</span></h3>
+					</div>
+					{/* <h3>MERN Expert </h3>
+					<span ref={textRef}></span> */}
+				</div>
+				<a href="#portfolio">
+					<img src="../../assets/down.png" alt="" />
+				</a>
+			</div>
+		</div>
+	);
 }
